edce05f3de7f8f6836dbdf180b2eefe184edfbfe
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-05-26 08:36:36 -0400

    Update build.sh

--- src/travis/build.sh
+++ src/travis/build.sh
@@ -1,7 +1,13 @@
+# Set up Apache
+bash DrupalCI/src/travis/apache.sh
+
+# Mail service fix
 sudo service postfix stop
 smtp-sink -d "%d.%H.%M.%S" localhost:2500 1000 &
 echo -e '#!/usr/bin/env bash\nexit 0' | sudo tee /usr/sbin/sendmail
 echo 'sendmail_path = "/usr/sbin/sendmail -t -i "' | sudo tee "/home/travis/.phpenv/versions/`php -i | grep "PHP Version" | head -n 1 | grep -o -P '\d+\.\d+\.\d+.*'`/etc/conf.d/sendmail.ini"
+
+# Install Drupal
 cd drupal-project
 vendor/bin/drush si standard -y --db-url=sqlsrv://sa:Password12!@localhost/mydrupalsite --clean-url=0 --account-name=admin --account-pass=drupal
 vendor/bin/drush en -y simpletest
